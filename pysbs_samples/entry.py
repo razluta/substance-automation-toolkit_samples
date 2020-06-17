29710aa6a821329c2343dd54d8deb73acd1186c8
Razvan Luta <razvan.luta@gmail.com>
2020-06-16 23:30:33 -0700

    Starting the setup for the python side of the samples.

---
+++ pysbs_samples/entry.py
@@ -0,0 +1,11 @@
+import pysbs
+
+# Defining the user customizable data
+USER_SETTINGS_FILE = "user_settings.json"
+CONFIG_NORMAL_MAP_FILE = "config_normal.json"
+CONFIG_AO_MAP_FILE = "config_ao.json"
+
+# Loading the user customizable data
+
+
+print USER_SETTINGS_FILE
